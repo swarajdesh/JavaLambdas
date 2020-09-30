7fe20fa3175b6d919b335afd17228c357fb7e187
swaraj <swarajdeshmukh45@gmail.com>
2020-10-01 01:20:43 +0530

    Closures

---
+++ src/unit2/ClosuresExample.java
@@ -0,0 +1,18 @@
+package unit2;
+
+public class ClosuresExample {
+
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 10;
+
+        doProcess(a, i -> System.out.println(i + b));
+    }
+
+    private static void doProcess(int i, Process p) {
+        p.Process(i);
+    }
+    interface Process{
+        void Process(int i);
+    }
+}
