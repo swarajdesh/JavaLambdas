4155f55a40d9fe674a1bc49c4ea723841337a02d
swaraj <swarajdeshmukh45@gmail.com>
2020-10-01 00:55:03 +0530

    ExceptionHandlingExample in lambda

--- src/unit2/ExceptionHandlingExample.java
+++ src/unit2/ExceptionHandlingExample.java
@@ -8,7 +8,7 @@ public class ExceptionHandlingExample {
         int[] scoreNumbers = {1,2,3,4,5};
         int key = 0;
 
-        process(scoreNumbers, key, (i,k) -> System.out.println(i / k));
+        process(scoreNumbers, key, wrapperLambda((v, k) -> System.out.println(v / k)));
     }
 
     private static void process(int[] scoreNumbers, int key, BiConsumer<Integer , Integer> consumer) {
@@ -16,6 +16,15 @@ public class ExceptionHandlingExample {
              scoreNumbers) {
             consumer.accept(i,key);
         }
-        
+    }
+
+    private static BiConsumer<Integer , Integer> wrapperLambda(BiConsumer<Integer , Integer> consumer){
+      return (v, k) -> {
+          try {
+              consumer.accept(v, k);
+          }catch (ArithmeticException e){
+              System.out.println("Exception caught in wrapper lambda");
+          }
+      };
     }
 }
